c40c3d61ac94304ef4e372f8e52df877ff4780f0
Karl Higley <kmhigley@gmail.com>
2012-12-21 17:59:10 -0500

    Basic application setup.
    Includes Postgres, HAML, RSpec, Factory Girl, and Capybara.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hacksession::Application.config.secret_token = 'f44f9736a70eb1027b4b624bd0dfc694977d36feaa659a6b213684e2ae1d388f9f35afaaae217f47fbede58732958c0d6d20b1c9d01897b26611facdc259b0e6'
