6f2cf125b11e1631f1f175ce09991e1c92d41e38
Karl Higley <kmhigley@gmail.com>
2013-02-08 15:21:54 -0500

    Restricted editing the announcements to logged in users.

--- spec/features/announcements_spec.rb
+++ spec/features/announcements_spec.rb
@@ -4,22 +4,35 @@ describe 'Managing announcements' do
 
   before :each do
     @announcement = FactoryGirl.create(:announcement)
+    visit root_path
   end
 
   it 'displays announcement text' do
-    visit root_path
     expect(page).to have_content @announcement.content
   end
 
-  it 'allows editing the announcement' do
-    attributes = FactoryGirl.attributes_for(:announcement)
+  context 'when not logged in' do
+    it "doesn't allow editing the announcement" do
+      expect(page).not_to have_link 'Edit Announcement'
+    end
+  end
 
-    visit root_path
-    click_link 'Edit Announcement'
-    fill_in 'announcement_content', :with => attributes[:content]
-    click_button 'Update Announcement'
+  context 'when logged in' do
+    before :each do
+      @user = FactoryGirl.create(:user)
+      login_as(@user, :scope => :user)
+      visit root_path
+    end
 
-    expect(page).to have_content attributes[:content]
+    it 'allows editing the announcement' do
+      attributes = FactoryGirl.attributes_for(:announcement)
+
+      click_link 'Edit Announcement'
+      fill_in 'announcement_content', :with => attributes[:content]
+      click_button 'Update Announcement'
+
+      expect(page).to have_content attributes[:content]
+    end
   end
 
 end
