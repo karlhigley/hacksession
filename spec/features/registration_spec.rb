6b0b9c2ad2b63fb7b4ff14e0ffcdebb8ed07725b
Karl Higley <kmhigley@gmail.com>
2013-02-07 20:50:14 -0500

    Added Twitter handle to registration form.

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -14,18 +14,21 @@ describe 'Registration' do
     click_link 'Register'
     expect(page).to have_field 'Name'
     expect(page).to have_field 'Email'
+    expect(page).to have_field 'Twitter Handle'
     expect(page).to have_field 'Password'
-    expect(page).to have_field 'Password confirmation'
+    expect(page).to have_field 'Password Confirmation'
     expect(page).to have_button 'Sign up'
   end
 
   it 'registers a new user' do
+    user_attr = FactoryGirl.attributes_for(:user)
     expect {
       click_link 'Register'
-      fill_in 'Name', :with => 'John Doe'
-      fill_in 'Email', :with => 'johndoe@example.com'
-      fill_in 'user_password', :with => 'password'
-      fill_in 'Password confirmation', :with => 'password'
+      fill_in 'Name', :with => user_attr[:name]
+      fill_in 'Email', :with => user_attr[:email]
+      fill_in 'Twitter Handle', :with => user_attr[:twitter]
+      fill_in 'user_password', :with => user_attr[:password]
+      fill_in 'Password Confirmation', :with => user_attr[:password]
       click_button 'Sign up'
     }.to change{ User.count }.by(1)
   end
